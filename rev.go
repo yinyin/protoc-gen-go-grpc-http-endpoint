640fb165e6e14cd71273aca03a1d5a85a5e25b91
Yinyin L <yinyinl@gmail.com>
2024-07-14 07:14:44 +0800

    add revision tracker

---
+++ rev.go
@@ -0,0 +1,7 @@
+package protocgenghe
+
+// CodeFullVersion contain full version text.
+const CodeFullVersion = "0.0.1; 6fd0cef91c98ee8a21364a51217f796b1cc01812" // REV-CONSTANT:full 5d022db7d38f580a850cd995e26a6c2f
+
+// CodeVersion contain short version text.
+const CodeVersion = "0.0.1" // REV-CONSTANT:rev 5d022db7d38f580a850cd995e26a6c2f
